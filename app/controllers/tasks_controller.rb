fd23408048fd9fcd65d5ff6841d26b97c62168f2
s.sato <s.sato8815@gmail.com>
2020-09-21 09:15:51 +0000

    taskCRUD

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = current_user.tasks.order(id: :desc).page(params[:page])
     # current_userでユーザーのインスタンス取得
@@ -44,16 +44,19 @@ class TasksController < ApplicationController
       @task.destroy
       
       flash[:success] = 'Task は正常に削除されました'
-      redirect_to tasks_url
+      redirect_back(fallback_location: root_path)
   end
 
   private
-
-  def set_task
-      @task = Task.find(params[:id])
-  end
   
   def task_params
       params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
